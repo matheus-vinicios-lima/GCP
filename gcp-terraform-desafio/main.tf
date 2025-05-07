32f9c65d6a4d6b5aa7ee9d3af5ab39d0794828b8
Matheus Lima <82986733+matheus-vinicios-lima@users.noreply.github.com>
2025-05-07 16:24:41 -0300

    Add files via upload

---
+++ gcp-terraform-desafio/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  backend "gcs" {
+    bucket  = "tf-bucket-377907"
+ prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.53.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+module "instances" {
+  source     = "./modules/instances"
+}
+
+module "storage" {
+  source     = "./modules/storage"
+}
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 6.0.0"
+
+    project_id   = var.project_id
+    network_name = "tf-vpc-385209"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = var.region
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = var.region
+        }
+    ]
+}
+
+resource "google_compute_firewall" "tf-firewall" {
+  name    = "tf-firewall"
+  network = "projects/qwiklabs-gcp-03-b1796eb5a58f/global/networks/tf-vpc-385209"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_tags = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
---
+++ gcp-terraform-desafio/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+ default = "us-east1"
+}
+
+variable "zone" {
+ default = "us-east1-c"
+}
+
+variable "project_id" {
+ default = "qwiklabs-gcp-03-b1796eb5a58f"
+}
